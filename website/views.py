d2941e1291d7fe800e34a51da1eb5cd6ed4ac8e7
Matěj Růžička <matejruzicka@Matejs-MacBook-Air.local>
2023-02-24 22:16:47 +0100

    feat(files): add jednaci rad vh and volebni rad vh

--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@ from website.utils import FileTypes, sort_members
 
 def home(request):
     board_members = sort_members()
-    files = File.objects.filter(slug__in=["prihlaska", "stanovy"])
+    files = File.objects.filter(slug__in=["prihlaska", "stanovy", "jednaci-rad-vh", "volebni-rad-vh"])
     return render(request, "home.html", {"board_members": board_members, "files": files})
 
 
--- website_en/views.py
+++ website_en/views.py
@@ -6,7 +6,7 @@ from website.utils import sort_members
 
 def home_en(request):
     board_members = sort_members()
-    files = File.objects.filter(slug__in=["prihlaska", "stanovy"])
+    files = File.objects.filter(slug__in=["prihlaska", "stanovy", "jednaci-rad-vh", "volebni-rad-vh"])
     return render(request, "home_en.html", {"board_members": board_members, "files": files})
 
 
